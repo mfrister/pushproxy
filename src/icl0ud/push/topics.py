7d8afad333873c532d901a4569244cc6b55f73f4
Michael Frister <michael+git@frister.net>
2013-02-14 17:17:26 +0100

    Add Apple push topic list

---
+++ src/icl0ud/push/topics.py
@@ -0,0 +1,29 @@
+from hashlib import sha1
+
+TOPICS = [
+    'com.apple.ess',
+    'com.apple.gamed',
+    'com.apple.itunesstored',
+    'com.apple.itunesu',
+    'com.apple.jalisco',
+    'com.apple.madrid',  # iMessage
+    'com.apple.mediastream.subscription.push',
+    'com.apple.mobileme.fmf1',  # Find My Friends
+    'com.apple.mobileme.fmip',  # Find My iPhone/Mac
+    'com.apple.sagad',
+    'com.apple.store.Jolly',
+    'com.me.bookmarks',
+    'com.me.btmm',  # Back To My Mac
+    'com.me.cal',
+    'com.me.contacts',
+    'com.me.keyvalueservice',
+    'com.me.setupservice',
+    'com.me.ubiquity',
+    'com.me.ubiquity.system',
+]
+
+TOPIC_HASHES = dict([(sha1(topic).digest(), topic) for topic in TOPICS])
+
+
+def topicForHash(hash):
+    return TOPIC_HASHES.get(hash, hash.encode('hex'))
